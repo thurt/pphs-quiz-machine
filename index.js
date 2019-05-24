841972f1b4305eaf1986d242c283ae6fec92ab2b
Taylor Hurt <taylor.a.hurt@gmail.com>
2019-05-24 11:25:22 -0400

    install and use cors

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require("cors");
 const port = process.env.PORT || 3000;
 
 const app = express();
 
 //app.use(express.static("./public"));
+app.use(cors());
 app.use(express.json());
 
 const quizzes = [
